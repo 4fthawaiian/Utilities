d4389cdee08ff9fde8d4d0273c00f3cbcd3076d5
Your Name <brad@mccroreys.com>
2011-11-23 12:16:50 -0600

    DO IT

---
+++ utils.js
@@ -0,0 +1,30 @@
+
+function MessageBar() {
+	var a = function (a, b) {
+		for (var c in b) {
+			a.style[c] = b[c];
+		}
+		return a
+	},
+		b = a(document.createElement("div"), {
+			top: 0,
+			left: 0,
+			position: "fixed",
+			background: "orange",
+			width: "100%",
+			padding: "10px",
+			textAlign: "center"
+		});
+	b.setAttribute('id', "alertMessage");
+	document.body.appendChild(b);
+	this.setMessage = function (a) {
+			while (b.firstChild) {
+				b.removeChild(b.firstChild)
+			};
+			b.appendChild(document.createTextNode(a))
+		};
+	this.toggleVisibility = function () {
+			b.style.display = b.style.display === "none" ? "block" : "none"
+		}
+}
+
