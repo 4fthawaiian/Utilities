24d3361a643fa4866f27ab5782d62761e1457e5b
Your Name <brad@mccroreys.com>
2011-12-20 15:41:47 -0600

    trying again

--- utils.js
+++ utils.js
@@ -6,27 +6,14 @@ MessageBar: function() {
 		for (var c in b) {
 			a.style[c] = b[c];
 		}
-		return a
-	},
-		b = a(document.createElement("div"), {
-			top: 0,
-			left: 0,
-			position: "fixed",
-			background: "orange",
-			width: "100%",
-			padding: "10px",
-			textAlign: "center"
-		});
+		return a;
+	};
+	var b = a(document.createElement("div"), { top: 0, left: 0, position: "fixed", background: "orange", width: "100%", padding: "10px", textAlign: "center" });
 	b.setAttribute('id', "alertMessage");
 	document.body.appendChild(b);
-	this.setMessage = function (a) {
-			while (b.firstChild) {
-				b.removeChild(b.firstChild)
-			};
-			b.appendChild(document.createTextNode(a))
-		};
-	this.toggleVisibility = function () {
-			b.style.display = b.style.display === "none" ? "block" : "none"
-		}
+	this.setMessage = function (a) { while (b.firstChild) { b.removeChild(b.firstChild) } b.appendChild(document.createTextNode(a)); };
+	this.toggleVisibility = function () { b.style.display = b.style.display === "none" ? "block" : "none"; };
 }
-}
+};
+var MessageBar = contegix.MessageBar;
+
