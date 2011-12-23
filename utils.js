11b07b153b0a218fb884ec2df88a55f93e9c0460
Bradley McCrorey <brad@mccroreys.com>
2011-12-22 18:47:29 -0600

    added GetHTTPTickets function to parse out a list of tickets and only return the HTTP alerts

--- utils.js
+++ utils.js
@@ -13,67 +13,34 @@ MessageBar: function() {
 	document.body.appendChild(b);
 	this.setMessage = function (a) { while (b.firstChild) { b.removeChild(b.firstChild) } b.appendChild(document.createTextNode(a)); };
 	this.toggleVisibility = function () { b.style.display = b.style.display === "none" ? "block" : "none"; };
-},
-parseJSON: (function (s) {
+}
+GetHTTPTickets: function() {
+var txt = $("table#ticket_list").text();
 
-  var m = {
-    '\b': '\\b',
-    '\t': '\\t',
-    '\n': '\\n',
-    '\f': '\\f',
-    '\r': '\\r',
-    '"' : '\\"',
-    '\\': '\\\\'
-  };
+var ticket_row = 0;
+var display_tickets = new Array();
+console.debug(display_tickets);
 
-  s.parseJSON = function (filter) {
-     /*
-           Reason: Why this function is useful?
-     */
-    // Parsing happens in three stages. In the first stage, we run the text against
-    // a regular expression which looks for non-JSON characters. We are especially
-    // concerned with '()' and 'new' because they can cause invocation, and '='
-    // because it can cause mutation. But just to be safe, we will reject all
-    // unexpected characters.
-
-    try {
-      if (/^("(\\.|[^"\\\n\r])*?"|[,:{}\[\]0-9.\-+Eaeflnr-u \n\r\t])+?$/.test(this)) {
-
-          // In the second stage we use the eval function to compile the text into a
-          // JavaScript structure. The '{' operator is subject to a syntactic ambiguity
-          // in JavaScript: it can begin a block or an object literal. We wrap the text
-          // in parens to eliminate the ambiguity.
-
-          var j = eval('(' + this + ')');
-
-          // In the optional third stage, we recursively walk the new structure, passing
-          // each name/value pair to a filter function for possible transformation.
-
-          if (typeof filter === 'function') {
-
-            function walk(k, v) {
-              if (v && typeof v === 'object') {
-                for (var i in v) {
-                  if (v.hasOwnProperty(i)) {
-                    v[i] = walk(i, v[i]);
-                  }
-                }
-              }
-              return filter(k, v);
-            }
-
-            j = walk('', j);
-          }
-          return j;
+$('table#ticket_list tr').each(function() {
+    if(ticket_row > 0) {
+      ticket_number = $(this).find("td:eq(1)").find("a:eq(1)").text();
+      ticket_subject_container = $(this).find("td:eq(1)");
+      ticket_subject_lines = ($(this).find("td:eq(1)").text().split('\n'));
+      ticket_subject = $.trim(ticket_subject_lines[(ticket_subject_lines.length - 2)]);
+      if(ticket_subject.search(/HTTP Availability/) > 0) {
+        engineer_lines = $(this).find("td:eq(3)").text().split('\n');
+        ticket_status = $(this).find("td:eq(4)").text();
+        engineer = $.trim(engineer_lines[1]);
+        engineer_status = "On Queue";
+        if ($.trim(engineer_lines[2]) !== "") {
+          engineer_status = $.trim(engineer_lines[2]);
         }
-      } catch (e) {
-
-  // Fall through if the regexp test fails.
-
+        display_tickets[display_tickets.length] = {"number": ticket_number, "subject": ticket_subject, "engineer": engineer, "status": ticket_status, "engineer_status": engineer_status}
       }
-      throw new SyntaxError("parseJSON: filter failed");
-    };
-  }
-)
+    }
+    ticket_row += 1;
+});
+return display_tickets;
+}
 };
 var MessageBar = contegix.MessageBar; // set up the function sans-class, to preserve legacy
